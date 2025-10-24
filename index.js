e970d359377aaf4b8cb83c5a0603fa17a00ec767
Akhilesh2006s <jahnaviparuchuri14@gmail.com>
2025-10-24 22:09:13 +0530

    Add latest Vercel frontend URL to CORS configuration

--- index.js
+++ index.js
@@ -109,6 +109,7 @@ app.use(cors({
     'https://alsi-stud-frontend-mf3r-ea1jir1t6-akhilesh2006s-projects.vercel.app',
     'https://alsi-stud-frontend-mf3r-r50hrstmi-akhilesh2006s-projects.vercel.app',
     'https://alsi-stud-frontend-mf3r-12gsssa10-akhilesh2006s-projects.vercel.app',
+    'https://alsi-stud-frontend-mf3r-gajkeubdu-akhilesh2006s-projects.vercel.app',
     'https://alsi-stud-frontend-mf3r.vercel.app',
     process.env.CLIENT_URL
   ].filter(Boolean),
